f32cc0221da7849e59ec4ed682a667c50f2e7f02
jalpesh vadgama <Jalpesh@dotnetjalps.com>
2016-06-18 19:11:57 +0530

    Employee migration

--- CoreMigration/src/CoreMigration/Data/Migrations/20160618133713_AddEmployee.Designer.cs
+++ CoreMigration/src/CoreMigration/Data/Migrations/20160618133713_AddEmployee.Designer.cs
@@ -8,9 +8,10 @@ using CoreMigration.Data;
 namespace CoreMigration.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160618133713_AddEmployee")]
+    partial class AddEmployee
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rc2-20901")
---
+++ CoreMigration/src/CoreMigration/Data/Migrations/20160618133713_AddEmployee.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoreMigration.Data.Migrations
+{
+    public partial class AddEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
